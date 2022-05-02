a6d1737ebbcd7b210490efde5bfad83598d0808d
Jakub Gugulski <jakub.gugulski1@gmail.com>
2022-05-03 00:39:32 +0200

    add slide animation

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-const burgerBtn = document.querySelector('.burger')
+const burger = document.querySelector('.burger')
+const barsBtn = document.querySelector('.fa-bars')
+const closeBtn = document.querySelector('.fa-times')
+const nav = document.querySelector('nav ul')
+const tempBtn = document.querySelector('.tempBtn')
+const calculator = document.querySelector('.calculator')
+const homeBtn = document.querySelector('.home')
+
+const menu = () => {
+    nav.classList.toggle('active')
+    burger.classList.toggle('active')
+    barsBtn.classList.toggle('hide')
+    closeBtn.classList.toggle('hide')
+}
+
+const calcSlide = () => {
+    calculator.classList.add('slide')
+    menu()
+}
+
+const home = () => {
+    calculator.classList.remove('slide')
+    menu()
+}
+
+
+burger.addEventListener('click', menu)
+tempBtn.addEventListener('click', calcSlide)
+homeBtn.addEventListener('click', home)
