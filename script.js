5abf68ad92c2b9e6c327ef9b995bfef61f7ce2f4
Jakub Gugulski <jakub.gugulski1@gmail.com>
2022-05-03 13:45:54 +0200

    add temp calc and second calculator template

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const barsBtn = document.querySelector('.fa-bars')
 const closeBtn = document.querySelector('.fa-times')
 const nav = document.querySelector('nav ul')
 const tempBtn = document.querySelector('.tempBtn')
-const calculator = document.querySelector('.calculator')
+const calcBtn = document.querySelector('.calcBtn')
+const calculator1 = document.querySelector('.calculator1')
+const calculator2 = document.querySelector('.calculator2')
 const homeBtn = document.querySelector('.home')
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convertBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const swapBtn = document.querySelector('.swap')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+let farenheit
+let celsius
+
+//slider
 
 const menu = () => {
     nav.classList.toggle('active')
@@ -14,12 +27,63 @@ const menu = () => {
 }
 
 const calcSlide = () => {
-    calculator.classList.add('slide')
+    calculator1.classList.add('slide')
+    calculator2.classList.remove('slide')
     menu()
 }
 
 const home = () => {
-    calculator.classList.remove('slide')
+    calculator1.classList.remove('slide')
+    calculator2.classList.remove('slide')
+    menu()
+}
+
+// temp calc
+
+const swap = () => {
+    if (one.textContent == 'Celsius') {
+        one.textContent = 'Farenheit'
+        two.textContent = 'Celsius'
+        result.textContent = ''
+    } else {
+        one.textContent = 'Celsius'
+        two.textContent = 'Farenheit'
+        result.textContent = ''
+    }
+}
+
+const celToFar = () => {
+    farenheit = converter.value * 1.8 + 32
+    result.textContent = `${converter.value}°C equals ${farenheit.toFixed(1)}°F`
+    converter.value = ''
+}
+
+const farToCel = () => {
+    celsius = (converter.value - 32) / 1.8
+    result.textContent = `${converter.value}°F equals ${celsius.toFixed(1)}°C`
+    converter.value = ''
+}
+
+const conversion = () => {
+    if (converter.value !== '') {
+        if (one.textContent === 'Celsius') {
+            celToFar()
+        } else {
+            farToCel()
+        }
+    } else {
+        result.textContent = 'No data!'
+    }
+}
+
+const reset = () => {
+    result.textContent = ''
+    converter.value = ''
+}
+
+const calc2Slide = () => {
+    calculator1.classList.remove('slide')
+    calculator2.classList.add('slide')
     menu()
 }
 
@@ -27,3 +91,7 @@ const home = () => {
 burger.addEventListener('click', menu)
 tempBtn.addEventListener('click', calcSlide)
 homeBtn.addEventListener('click', home)
+swapBtn.addEventListener('click', swap)
+convertBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
+calcBtn.addEventListener('click', calc2Slide)
